cb1287eb87b52a2915e0042612f4ee717bee6e25
Kariya44 <Kariya.41194119@gmail.com>
2024-01-04 10:10:29 +0900

    oauth/callbackアクションのrouting設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'oauths/oauth'
-  get 'oauths/callback'
-  root "static_pages#top"
+  
   resources :users, only: %i[new create]
 
+  # Google認証
+  post "oauth/callback" => "oauths#callback"
+  get "oauth/callback" => "oauths#callback" 
+  get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
+
+  # ユーザー登録
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
