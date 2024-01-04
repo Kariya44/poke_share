0d939ee1ecae8c02552a4ecf0870f190d80710a4
Kariya44 <Kariya.41194119@gmail.com>
2024-01-04 11:10:33 +0900

    投稿一覧画面のルーティングを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   accepts_nested_attributes_for :authentications
   has_many :authentications, :dependent => :destroy
+  has_many :boards, dependent: :destroy
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
